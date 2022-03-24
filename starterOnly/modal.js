b7bdcf2e605e10318bedcba0a18cd6faa67e5eda
Alex <alexperrier.pro@gmail.com>
2022-03-24 16:30:08 +0100

    edit(js) : error messages

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,24 +10,54 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const closeBtn = document.querySelector(".close");
+const closeBtn = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
+const form = document.getElementById("form");
 
-// launch modal event
+// Launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+// Close modal event
+closeBtn.forEach((btnClose) => btnClose.addEventListener("click", closeModal));
+
+// Launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-// close modal event
-closeBtn.addEventListener("click", closeModal);
-
-// close modal form
-function closeModal(e) {
-  e.stopPropagation();
+// Close modal form
+function closeModal() {
   modalbg.style.display = "none";
 }
+
+// Add error message
+function addErrorMsg(elementId, errorMessage) {
+  document
+    .getElementById(elementId)
+    .setAttribute("data-error-visible", true);
+  document
+    .getElementById(elementId)
+    .setAttribute("data-error", errorMessage);
+}
+
+// Remove error message
+function removeErrorMsg(elementId) {
+  document
+    .getElementById(elementId)
+    .setAttribute("data-error-visible", false);
+  document
+    .getElementById(elementId)
+    .setAttribute("data-error", "");
+}
+
+
+
+
+
+
+
+
+
+
 
 
